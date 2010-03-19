f42c535a11b887b310f73007c6c2d1dcec3a804b
Riku Voipio <riku.voipio@nokia.com>
2010-03-19 11:19:58 +0200

    add multi-sd support
    N900 and other devices can have more than one sd/mmc card, and thus
    the -sd command line option needs to be read multiple times.

--- vl.c
+++ vl.c
@@ -627,7 +627,7 @@ static int bt_parse(const char *opt)
 #define FD_ALIAS "index=%d,if=floppy"
 #define PFLASH_ALIAS "if=pflash"
 #define MTD_ALIAS "if=mtd"
-#define SD_ALIAS "index=0,if=sd"
+#define SD_ALIAS "index=%d,if=sd"
 
 static int drive_init_func(QemuOpts *opts, void *opaque)
 {
@@ -1912,6 +1912,7 @@ int main(int argc, char **argv, char **envp)
     const char *incoming = NULL;
     int show_vnc_port = 0;
     int defconfig = 1;
+    int sd_device_index = 0;
 
 #ifdef CONFIG_SIMPLE_TRACE
     const char *trace_file = NULL;
@@ -2058,7 +2059,7 @@ int main(int argc, char **argv, char **envp)
                 drive_add(optarg, MTD_ALIAS);
                 break;
             case QEMU_OPTION_sd:
-                drive_add(optarg, SD_ALIAS);
+                drive_add(optarg, SD_ALIAS, sd_device_index++);
                 break;
             case QEMU_OPTION_pflash:
                 drive_add(optarg, PFLASH_ALIAS);
@@ -2903,7 +2904,7 @@ int main(int argc, char **argv, char **envp)
 
     if (default_sdcard) {
         /* we always create one sd slot, even if no card is in it */
-        drive_add(NULL, SD_ALIAS);
+        drive_add(NULL, SD_ALIAS, 0);
     }
 
     /* open the virtual block devices */
